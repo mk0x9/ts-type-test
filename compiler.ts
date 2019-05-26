d39b0d1421dab91734280040feb70290d107bc87
Michael Kuryshev <me@mk9.name>
2019-05-26 23:06:55 +0200

    pass directory as argument

--- compiler.ts
+++ compiler.ts
@@ -1,4 +1,6 @@
 import ts from 'typescript';
+import path from 'path';
+import process from 'process';
 
 export function getProgram(): ts.Program {
     // https://stackoverflow.com/a/53898219
@@ -8,14 +10,23 @@ export function getProgram(): ts.Program {
         readDirectory: ts.sys.readDirectory,
         useCaseSensitiveFileNames: true
     };
-    const configFileName = ts.findConfigFile('./', ts.sys.fileExists, 'tsconfig.json');
+
+    const configFileName = ts.findConfigFile(
+        path.resolve(process.cwd(), process.argv[2]),
+        ts.sys.fileExists,
+        'tsconfig.json'
+    );
 
     if (!configFileName) {
         throw new Error("Can't find tsconfig.json!");
     }
 
     const configFile = ts.readConfigFile(configFileName, ts.sys.readFile);
-    const parsedCommandLine = ts.parseJsonConfigFileContent(configFile.config, parseConfigHost, './');
+    const parsedCommandLine = ts.parseJsonConfigFileContent(
+        configFile.config,
+        parseConfigHost,
+        path.dirname(configFileName)
+    );
     const program = ts.createProgram(parsedCommandLine.fileNames, parsedCommandLine.options);
 
     return program;
